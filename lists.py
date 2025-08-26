39415dff30b87d77e4658971e6e84a421e8b83b7
Kassim Hassan <kassim.hassan@go.minneapolis.edu>
2025-08-26 15:16:34 -0500

    first commit

---
+++ lists.py
@@ -0,0 +1,12 @@
+libraries = ['pandas', 'numpy', 'flask']
+print(libraries)
+
+for lib in libraries:
+    print(lib)
+
+things_in_list = len(libraries)
+print(things_in_list)
+
+print(libraries[0])
+print(libraries[-1])
+print(count(libraries))
